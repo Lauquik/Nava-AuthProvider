e5618e4a08e1382ee778da53bc61280ae02a8978
Lauquik <laukik.22010776@viit.ac.in>
2024-01-29 00:24:25 +0530

    vercel config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './auth/SignIn';
 import SignUp from './auth/Signup';
 import AppLayout from './PrivateRoute';
-const App: React.FC = () => (
+
+function App() {
+	return (
 	<BrowserRouter>
-		<Routes>		
-			<Route element={<AppLayout />}>
-				<Route path="/signin" element={<SignIn />} />
-				<Route path="/signup" element={<SignUp />} />
-			</Route>
+		<Routes>
+		<Route element={<AppLayout />}>
+			<Route path="/signin" element={<SignIn />} />
+			<Route path="/signup" element={<SignUp />} />
+		</Route>
 		</Routes>
 	</BrowserRouter>
-);
+	);
+};
 
 export default App;
